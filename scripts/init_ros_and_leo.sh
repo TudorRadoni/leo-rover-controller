95f6513cf57f5f7bb37f8dddaca2c1de98794427
Tudor Radoni <tudor.radoni@gmail.com>
2023-12-29 23:41:51 +0200

    Add init and launch scripts

---
+++ scripts/init_ros_and_leo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Setup ROS and Gazebo
+source /opt/ros/noetic/setup.bash
+source /usr/share/gazebo-11/setup.sh
+
+# Setup Leo
+source /home/tudor/leo_ws/devel/setup.bash
---
+++ scripts/launch_leo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Store current directory and change to the target directory
+pushd ~/leo_ws/src/leo_simulator/leo_gazebo/launch/ > /dev/null
+
+# If no arguments are provided, list all available worlds
+if [ $# -eq 0 ]; then
+    echo "Available worlds:"
+    for file in *.launch; do
+        echo "${file%.*}"
+    done
+else
+    # Parse arguments
+    while getopts "w:" opt; do
+        case ${opt} in
+            w)
+                world=$OPTARG
+                ;;
+            \?)
+                echo "Invalid option: -$OPTARG" 1>&2
+                ;;
+        esac
+    done
+
+    # Start Leo in the specified world
+    if [ -n "$world" ]; then
+        roslaunch leo_gazebo "${world}.launch"
+    else
+        echo "Please specify a world with the -w option."
+    fi
+fi
+
+# Return to the original directory
+popd > /dev/null

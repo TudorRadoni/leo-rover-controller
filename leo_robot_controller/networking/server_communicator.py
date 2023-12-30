2a25494102e0032f4c3813adf3a9e96a7655ac92
Tudor Radoni <tudor.radoni@gmail.com>
2023-12-31 01:10:28 +0200

    Add communication with server

---
+++ leo_robot_controller/networking/server_communicator.py
@@ -0,0 +1,23 @@
+import socket
+import struct
+
+
+class ServerCommunicator:
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+
+    def send_data(self, x, z):
+        s = socket.socket()  # Create a client socket        
+        s.connect((self.server_address, self.server_port)) # Connect to the server
+
+        data = struct.pack('ii', x, z)  # Pack the data into a binary format
+
+        s.send(data)  # Send the data to the server
+        s.close()  # Close the socket
+
+    def send_quit_command(self):
+        # Pack a special quit command
+        # Use a special value to indicate quit
+        # 40000 is outside the range of the joystick
+        self.send_data(-40000, -40000)
